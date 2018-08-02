e70f54082c37d96592d8483b6324ec6ea097e3f3
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-08-02 15:24:09 -0400

    reverting to just game

--- project/app/models/api.rb
+++
@@ -1,9 +0,0 @@
-class Api < ActiveRecord::Base
-
-@url_array = []
-  def self.urlArray
-    url = 'https://api.spotify.com/v1/search?type=artist&q=tycho'
-    response = HTTParty.get(url)
-    url_hash << response.parsed_response
-
-  end
